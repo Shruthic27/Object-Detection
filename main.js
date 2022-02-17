af66eb42840bd19907261a21d7f5f864af8ae6e4
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2022-02-17 11:07:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function mtd(){
+window.location="desk.html"
+}
+
+function mtl(){
+window.location="livingroom.html"
+}
+
+function mtk(){
+window.location="kitchen.html"
+}
+
+function mtv(){
+window.location="vase.html"
+}
+
+function mtb(){
+window.location="bedroom.html"
+}
